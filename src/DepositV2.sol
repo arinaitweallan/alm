db28c63bd5aa2c39108ff9e78a8cb43acd1d8024
arinaitweallan <allan.bnb2@gmail.com>
2026-04-06 22:38:57 +0300

    added interface

--- src/DepositV2.sol
+++ src/DepositV2.sol
@@ -3,7 +3,10 @@ pragma solidity 0.8.30;
 
 import {UniswapV3Pool} from "lib/v3-core/contracts/UniswapV3Pool.sol";
 
-contract DepositV2 {
+// internal imports
+import {IDepositV2} from "src/interfaces/IDepositV2.sol";
+
+contract DepositV2 is IDepositV2 {
     UniswapV3Pool public immutable POOL;
 
     constructor(UniswapV3Pool _pool) {
